3ea955d0961e2845cf32bab45464429d695c3bf1
Arthur Pesah <arthur.pesah@gmail.com>
2020-11-30 22:07:00 +0100

    Implement plotting function for sex

--- process_covid.py
+++ process_covid.py
@@ -38,20 +38,45 @@ def generate_data_plot_confirmed(input_data, sex, max_age, status):
     max_age: sums all bins below this value, including the one it is in.
     status: 'new' or 'total' (default: 'total')
     """
-    raise NotImplementedError
+
+    # if sex is not None and max_age is not None:
+    #     raise ValueError("'sex' and 'max_age' cannot be specified at the same time")
+    # if not sex and not max_age:
+    #     raise ValueError("Either sex or max_ages should be specified")
+
+    list_dates = list(input_data["evolution"].keys())
+    list_confirmed_cases = []
+    if sex is not None:
+        if sex not in ["male", "female"]:
+            raise ValueError("The argument 'sex' should be either 'male' or 'female'")
+
+        for date in list_dates:
+            n_confirmed_cases = input_data["evolution"][date]["epidemiology"]["confirmed"][status][sex]
+            list_confirmed_cases.append(n_confirmed_cases)
+
+    return list_dates, list_confirmed_cases
 
 
-def create_confirmed_plot(input_data, sex=False, max_ages=[], status=..., save=...):
-    # FIXME check that only sex or age is specified.
+def create_confirmed_plot(input_data, sex=False, max_ages=[], status="total", save=...):
+    color = {"male": "#00C4AA",
+             "female": "#8700F9"}
+
+    if sex and len(max_ages) > 0:
+        raise ValueError("'sex' and 'max_ages' cannot be specified at the same time")
+    if not sex and len(max_ages) == 0:
+        raise ValueError("Either sex or max_ages should be specified")
+
     fig = plt.figure(figsize=(10, 10))
-    # FIXME change logic so this runs only when the sex plot is required
-    for sex in ['male', 'female']:
-        # FIXME need to change `changeme` so it uses generate_data_plot_confirmed
-        plt.plot('date', 'value', changeme)
-    # FIXME change logic so this runs only when the age plot is required
-    for age in max_ages:
-        # FIXME need to change `changeme` so it uses generate_data_plot_confirmed
-        plt.plot('date', 'value', changeme)
+
+    if sex:
+        for sex_type in ['male', 'female']:
+            list_dates, list_confirmed_cases = generate_data_plot_confirmed(input_data, sex_type, None, status)
+            plt.plot(list_dates, list_confirmed_cases, label=f"{status} {sex_type}", color=color[sex_type])
+    # else:
+        # for age in max_ages:
+            # FIXME need to change `changeme` so it uses generate_data_plot_confirmed
+            # plt.plot('date', 'value', changeme)
+
     fig.autofmt_xdate()  # To show dates nicely
     # TODO add title with "Confirmed cases in ..."
     # TODO Add x label to inform they are dates
@@ -59,6 +84,9 @@ def create_confirmed_plot(input_data, sex=False, max_ages=[], status=..., save=.
     # TODO Add legend
     # TODO Change logic to show or save it into a '{region_name}_evolution_cases_{type}.png'
     #      where type may be sex or age
+    plt.xlabel("Date")
+    plt.ylabel("# Cases")
+    plt.legend()
     plt.show()
 
 
