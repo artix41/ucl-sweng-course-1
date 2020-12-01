cef1333087cd6b7d33619bc424f6fda7c9234be2
Arthur Pesah <arthur.pesah@gmail.com>
2020-12-01 21:32:18 +0100

    Implement merging of age binning

--- process_covid.py
+++ process_covid.py
@@ -2,6 +2,63 @@ import json
 import matplotlib.pyplot as plt
 
 
+def merge_age_binning(age_binning1, age_binning2):
+    min_ages = ([int(age_range.split("-")[0]) for age_range in age_binning1],
+                [int(age_range.split("-")[0]) for age_range in age_binning2])
+
+    idx = [0, 0]
+    cur_idx = 0
+    list_id1, list_id2 = [], []
+
+    idx[cur_idx] += 1
+    while idx[0] < len(min_ages[0]) and idx[1] < len(min_ages[1]):
+        if min_ages[0][idx[0]] == min_ages[1][idx[1]]:
+            list_id1.append(idx[0] - 1)
+            list_id2.append(idx[1] - 1)
+        elif min_ages[cur_idx][idx[cur_idx]] > min_ages[1-cur_idx][idx[1-cur_idx]]:
+            cur_idx = 1 - cur_idx
+        idx[cur_idx] += 1
+
+    if len(list_id1) == 0:
+        raise ValueError(f"The age binning {age_binning1} and {age_binning2} could not be merged")
+    else:
+        list_id1.append(len(age_binning1) - 1)
+        list_id2.append(len(age_binning2) - 1)
+
+    return list_id1, list_id2
+
+
+def sum_sublists(l, indices):
+    if max(indices) >= len(l):
+        raise ValueError("'indices' contains elements greater than the size of 'l'")
+    result = []
+    subsum = 0
+    for i in range(len(l)):
+        subsum += l[i]
+        if i in indices:
+            result.append(subsum)
+            subsum = 0
+    return result
+
+
+def get_new_age_binning(init_age_binning, indices):
+    if max(indices) >= len(init_age_binning):
+        raise ValueError("'indices' contains elements greater than the size of 'l'")
+
+    cur_min = 0
+    result = []
+    change_cur_min = False
+    for i, bin in enumerate(init_age_binning):
+        if change_cur_min:
+            cur_min = bin.split("-")[0]
+            change_cur_min = False
+        if i in indices:
+            max_bin = "" if len(bin.split("-")) == 0 else bin.split("-")[1]
+            result.append(f"{cur_min}-{max_bin}")
+            change_cur_min = True
+    return result
+
+
 def load_covid_data(filepath):
     with open(filepath) as json_file:
         data = json.load(json_file)
@@ -10,13 +67,27 @@ def load_covid_data(filepath):
 
 def cases_per_population_by_age(input_data):
     total_population = input_data["region"]["population"]["age"]
-    age_binning = input_data["metadata"]["age_binning"]["population"]
-    result = {age_binning[i]: [] for i in range(len(age_binning))}
+    if None in total_population:
+        raise ValueError("Missing data in 'region'->'population'->'age'")
+    age_binning_population = input_data["metadata"]["age_binning"]["population"]
+    age_binning_cases = input_data["metadata"]["age_binning"]["hospitalizations"]
+
+    list_id1, list_id2 = merge_age_binning(age_binning_population, age_binning_cases)
+    print(age_binning_population, age_binning_cases)
+    new_total_population = sum_sublists(total_population, list_id1)
+    new_age_binning = get_new_age_binning(age_binning_population, list_id1)
+
+    result = {new_age_binning[i]: [] for i in range(len(new_age_binning))}
     for date in input_data["evolution"].keys():
-        for idx_age in range(4):
-            cases = input_data["evolution"][date]["epidemiology"]["confirmed"]["total"]["age"][idx_age]
-            percentage = cases / total_population[idx_age]
-            result[age_binning[idx_age]].append((date, percentage))
+        total_cases = input_data["evolution"][date]["epidemiology"]["confirmed"]["total"]["age"]
+        if None in total_cases:
+            raise ValueError(f"Missing data in 'evolution'->{date}->'epidemiology'->'confirmed'->'total'->'age'")
+
+        new_total_cases = sum_sublists(total_cases, list_id2)
+
+        for idx_age in range(len(new_total_cases)):
+            percentage = new_total_cases[idx_age] / new_total_population[idx_age]
+            result[new_age_binning[idx_age]].append((date, percentage))
     return result
 
 
--- test_process_covid.py
+++ test_process_covid.py
@@ -1,11 +1,11 @@
 from pytest import raises, approx
-from process_covid import compute_running_average
+from process_covid import *
 
 
 def equality_list(list_actual, list_expected):
     assert len(list_actual) == len(list_expected)
     assert all([type(a) == type(b) for a, b in zip(list_actual, list_expected)])
-    assert all([a == approx(b, 0.001) if isinstance(a, float) else True for a, b in zip(list_actual, list_expected)])
+    assert all([a == approx(b, 0.001) if isinstance(a, float) else a == b for a, b in zip(list_actual, list_expected)])
 
 
 def test_compute_running_average():
@@ -14,3 +14,81 @@ def test_compute_running_average():
 
     equality_list(compute_running_average([0, 1, 5, 2, 2, 5], 3), [None, 2.0, 2.6666, 3.0, 3.0, None])
     equality_list(compute_running_average([2, None, 4], 3), [None, 3.0, None])
+
+
+def test_simple_derivative():
+    equality_list(simple_derivative([None, 1, 2, None, 4]), [None, None, 1, None, None])
+
+
+def test_sum_sublists():
+    with raises(ValueError) as exception:
+        sum_sublists([1., 2., 3., 5.], [1, 2, 4])
+    equality_list(sum_sublists([1., 2., 3., 5.], [1, 3]), [3., 8.])
+
+
+def test_merge_binning():
+    with raises(ValueError) as exception:
+        merge_age_binning(['0-14', '15-29', '30-44', '45-'], ['0-19', '20-39', '40-'])
+    with raises(ValueError) as exception:
+        merge_age_binning(['0-'], ['0-30', '30-'])
+    with raises(ValueError) as exception:
+        merge_age_binning(['0-'], ['0-'])
+
+
+    list_id1, list_id2 = merge_age_binning(['0-14', '15-29', '20-44', '45-'], ['0-14', '15-29', '20-44', '45-'])
+    equality_list(list_id1, [0, 1, 2, 3])
+    equality_list(list_id2, [0, 1, 2, 3])
+
+    list_id1, list_id2 = merge_age_binning(['0-19', '20-39', '40-'], ['0-9', '10-39', '40-49', '50-'])
+    equality_list(list_id1, [1, 2])
+    equality_list(list_id2, [1, 3])
+
+    list_id1, list_id2 = merge_age_binning(['0-9', '10-39', '40-49', '50-'], ['0-19', '20-39', '40-'])
+    equality_list(list_id1, [1, 3])
+    equality_list(list_id2, [1, 2])
+
+    list_id1, list_id2 = merge_age_binning(['0-19', '20-39', '40-59', '60-'], ['0-29', '30-59', '60-'])
+    equality_list(list_id1, [2, 3])
+    equality_list(list_id2, [1, 2])
+
+    list_id1, list_id2 = merge_age_binning(['0-9', '10-19', '20-29', '30-39', '40-49', '50-'], ['0-19', '20-39', '40-'])
+    equality_list(list_id1, [1, 3, 5])
+    equality_list(list_id2, [0, 1, 2])
+
+
+def test_get_new_age_binning():
+    binning = get_new_age_binning(['0-9', '10-19', '20-29', '30-39', '40-49', '50-'], [1, 3, 5])
+    equality_list(binning, ["0-19", "20-39", "40-"])
+    binning = get_new_age_binning(['0-9', '10-19', '20-29', '30-39', '40-49', '50-'], [0, 5])
+    equality_list(binning, ["0-9", "10-"])
+    binning = get_new_age_binning(['0-9', '10-19', '20-29', '30-39', '40-49', '50-'], [0, 1, 2, 3, 4, 5])
+    equality_list(binning, ['0-9', '10-19', '20-29', '30-39', '40-49', '50-'])
+    binning = get_new_age_binning(['0-19', '20-39', '40-'], [1, 2])
+    equality_list(binning, ['0-39', '40-'])
+
+
+def test_cases_per_population_by_age():
+    input_data = {"metadata": {"age_binning": {"population": ['0-19', '20-39', '40-'],
+                                               "hospitalizations": ['0-9', '10-39', '40-49', '50-']}},
+                  "region": {"population": {"age": [100, 200, 800]}},
+                  "evolution": {"01-01-2020":
+                                    {"epidemiology": {"confirmed": {"total": {"age": [10, 50, 20, 60]}}}},
+                                "01-02-2020":
+                                    {"epidemiology": {"confirmed": {"total": {"age": [20, 100, 20, 60]}}}}}}
+    result = cases_per_population_by_age(input_data)
+
+    assert result == {'0-39': [("01-01-2020", 0.2), ("01-02-2020", 0.4)],
+                      '40-': [("01-01-2020", 0.1), ("01-02-2020", 0.1)]}
+
+    input_data = {"metadata": {"age_binning": {"population": ['0-19', '20-39', '40-'],
+                                               "hospitalizations": ['0-9', '10-39', '40-49', '50-']}},
+                  "region": {"population": {"age": [None, 200, 800]}},
+                  "evolution": {"01-01-2020":
+                                    {"epidemiology": {"confirmed": {"total": {"age": [10, 50, 20, 60]}}}},
+                                "01-02-2020":
+                                    {"epidemiology": {"confirmed": {"total": {"age": [20, 100, 20, 60]}}}}}}
+    with raises(ValueError) as exception:
+        cases_per_population_by_age(input_data)
+
+    assert result == {'0-39': [("01-01-2020", 0.2), ("01-02-2020", 0.4)],
+                      '40-': [("01-01-2020", 0.1), ("01-02-2020", 0.1)]}
